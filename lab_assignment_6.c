ac22ae2c5edc251832720b7196834ad57a3fa9b4
MikeUCF <130028967+MikeUCF@users.noreply.github.com>
2023-06-24 20:47:14 -0400

    Update lab_assignment_6.c
    Add Binary Search implementation to the search function.

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,7 +2,18 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if(low > high)
+		return -1;
+	
+	int mid = (high+low)/2;
+	if(numbers[mid] == value)
+		return mid;
+	else if(value > numbers[mid])
+		return search(numbers, mid+1, high, value);
+	else if(value < numbers[mid])
+		return search(numbers, low, mid-1, value);
+	else
+		return -1;
 }
 
 void printArray(int numbers[], int sz)
